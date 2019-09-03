d94559de8823d9458f8271e575f47c32ee785338
GionRubitschung <gion.rubitschuung@swisscom.com>
2019-09-03 14:41:43 +0200

    fixed style error and started with crud

--- src/app/content/content-components/home/home.component.ts
+++ src/app/content/content-components/home/home.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { LebenslaufService } from '../../../backend/service/lebenslauf/lebenslauf.service';
 import { PersonalData } from '../../../backend/service/interfaces/personal-data';
 import { AboutMe } from '../../../backend/service/interfaces/about-me';
 import {MatBottomSheet} from '@angular/material/bottom-sheet';
 import { BottomYeetComponent } from './bottom-yeet/bottom-yeet.component';
+import { AuthService } from 'src/app/authentification/auth.service';
+import { Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class HomeComponent implements OnInit {
 
   DATA: PersonalData[] = [];
   ICH: AboutMe[] = [];
   projekt: '';
+  loggedIn: boolean = false;
 
   // tslint:disable-next-line:variable-name
-  constructor(public dataService: LebenslaufService, private bottomSheet: MatBottomSheet, private YeetComp: BottomYeetComponent) { }
+  constructor(public dataService: LebenslaufService, private bottomSheet: MatBottomSheet, private YeetComp: BottomYeetComponent, private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.dataService.getData().subscribe(data => {
@@ -33,4 +43,15 @@ export class HomeComponent implements OnInit {
     this.bottomSheet.open(BottomYeetComponent);
   }
 
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isLoggedIn !== true) {
+      // this.router.navigate(['login']);
+      return false;
+    }
+    return true;
+  }
+
 }
+
